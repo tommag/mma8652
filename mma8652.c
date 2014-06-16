742d863d18452ee2c1e4a01bc2e14baf221419eb
tommag <magnier.tom@gmail.com>
2014-06-16 17:57:06 +0200

    Initial commit - basic structure of files

---
+++ mma8652.c
@@ -0,0 +1,6 @@
+/* MMA8652 C library - small 3-axis digital accelerometer from Freescale
+ * 
+ * 2014 - Tom Magnier : tom@tmagnier.fr
+ */
+ 
+ 
---
+++ mma8652.h
@@ -0,0 +1,11 @@
+/* MMA8652 C library - small 3-axis digital accelerometer from Freescale
+ * 
+ * 2014 - Tom Magnier : magnier.tom@gmail.com
+ */
+
+#ifndef MMA8652_H
+#define MMA8652_H
+
+
+
+#endif //MMA8652_H
